e9850edaa1757d0b89067ea26826fd18c41f211b
Samuel Cardoso <samuel.cardoso@zup.com.br>
2020-04-23 11:50:03 -0300

    more JNA tests

--- serverless-quarkus/src/main/java/org/zup/quarkus/ProcessingService.java
+++ serverless-quarkus/src/main/java/org/zup/quarkus/ProcessingService.java
@@ -13,6 +13,11 @@ public class ProcessingService {
         if (input.getName().equals("Stuart")) {
             throw new IllegalArgumentException(CAN_ONLY_GREET_NICKNAMES);
         }
+        // System.setProperty("java.library.path", "/home/samuel/desenv/workspaces/zup/si2/nativesamples/serverless-quarkus/src/META-INF/resources/");
+        // String dir = System.getProperty("user.dir");
+        // System.out.println("current dir = " + dir);
+        // String filePath = getClass().getClassLoader().getResource("libsomadorJNA.so").getFile();
+        // System.out.println("path = " + filePath);
         CalculadoraJNA calculadora = (CalculadoraJNA) Native.load("libsomadorJNA.so", CalculadoraJNA.class);
         String result = "Saida: " + calculadora.soma(1, 2);
         OutputObject out = new OutputObject();
